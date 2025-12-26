34a82ecc57c41f856a0d03b1dfc6fb05ef29ea78
Almina Ildirar <almina.ildirar@adesso.com.tr>
2025-12-26 21:38:53 +0300

    show employees list with http and modern control flow

--- src/app/features/employees/employees.page.ts
+++ src/app/features/employees/employees.page.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Employee, EmployeesApi } from '../../core/services/employees.api';
 
 @Component({
   standalone: true,
-  template: `
-    <h1>Employees</h1>
-    <p>Employee list will be here.</p>
-  `,
+  templateUrl: './employees.page.html',
+  styleUrl: './employees.page.scss',
 })
-export class EmployeesPage {}
+export class EmployeesPage {
+  private readonly api = inject(EmployeesApi);
+
+  readonly isLoading = signal(true);
+  readonly error = signal('');
+  readonly employees = signal<Employee[]>([]);
+
+  constructor() {
+    this.load();
+  }
+
+  load() {
+    this.isLoading.set(true);
+    this.error.set('');
+
+    this.api.getAll().subscribe({
+      next: (data) => {
+        this.employees.set(data);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.error.set('Employees could not be loaded.');
+        this.isLoading.set(false);
+      },
+    });
+  }
+}
