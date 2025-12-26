b2d8892490683f7ab2189b82901a4a6076ece390
Almina Ildirar <almina.ildirar@adesso.com.tr>
2025-12-26 19:48:41 +0300

    configure lazy loaded routes with shell layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './shared/ui/shell/sheel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/dashboard.page').then(
+            (m) => m.DashboardPage
+          ),
+      },
+      {
+        path: 'employees',
+        loadComponent: () =>
+          import('./features/employees/employees.page').then(
+            (m) => m.EmployeesPage
+          ),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./features/settings/settings.page').then(
+            (m) => m.SettingsPage
+          ),
+      },
+    ],
+  },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login.page').then((m) => m.LoginPage),
+  },
+
+  { path: '**', redirectTo: 'dashboard' },
+];
