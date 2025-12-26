5c2519a6cae3494d052814019bb427fb2075cbc2
Almina Ildirar <almina.ildirar@adesso.com.tr>
2025-12-26 22:06:36 +0300

    use EmployeesStore in employees list page

--- src/app/features/employees/employees.page.ts
+++ src/app/features/employees/employees.page.ts
@@ -1,6 +1,6 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Employee, EmployeesApi } from '../../core/services/employees.api';
+import { EmployeesStore } from '../../core/services/employee.store';
 
 @Component({
   standalone: true,
@@ -9,29 +9,5 @@ import { Employee, EmployeesApi } from '../../core/services/employees.api';
   styleUrl: './employees.page.scss',
 })
 export class EmployeesPage {
-  private readonly api = inject(EmployeesApi);
-
-  readonly isLoading = signal(true);
-  readonly error = signal('');
-  readonly employees = signal<Employee[]>([]);
-
-  constructor() {
-    this.load();
-  }
-
-  load() {
-    this.isLoading.set(true);
-    this.error.set('');
-
-    this.api.getAll().subscribe({
-      next: (data) => {
-        this.employees.set(data);
-        this.isLoading.set(false);
-      },
-      error: () => {
-        this.error.set('Employees could not be loaded.');
-        this.isLoading.set(false);
-      },
-    });
-  }
+  readonly store = inject(EmployeesStore);
 }
