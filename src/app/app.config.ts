9a44a9e2c8931d4c339ecd6279f41dc08048796b
Almina Ildirar <almina.ildirar@adesso.com.tr>
2025-12-26 21:34:50 +0300

    enable HttpClient and register interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authHeaderInterceptor } from './core/http/auth-header.interceptor';
+import { errorInterceptor } from './core/http/error.interceptor';
+import { mockApiInterceptor } from './core/http/mock-api.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+
+    provideHttpClient(
+      withInterceptors([
+        mockApiInterceptor,
+        authHeaderInterceptor,
+        errorInterceptor,
+      ])
+    ),
+  ],
 };
